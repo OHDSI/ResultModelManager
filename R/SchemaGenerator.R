c7bf3e66a4b8d02a4463d509a98ae36b0e44dde9
Jamie Gilbert <JGilber2@its.jnj.com>
2022-12-02 14:32:18 -0500

    Releas version 0.1.1
    * schema gen utility 
    
    * setting ConnectionHandler self$snakeCaseToCamelCase
    
    * readr dependency

---
+++ R/SchemaGenerator.R
@@ -0,0 +1,92 @@
+# Copyright 2022 Observational Health Data Sciences and Informatics
+#
+# This file is part of CohortDiagnostics
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+.writeFieldDefinition <- function(field) {
+  field <- as.list(field)
+  str <- paste("\t", field$columnName, toupper(field$dataType))
+
+  if (field$primaryKey == "yes") {
+    str <- paste(str, "NOT NULL")
+  }
+
+  str
+}
+
+#' Schema generator
+#' @description
+#' Take a csv schema definition and create a basic sql script with it.
+#'
+#' @param csvFilepath                   Path to schema file. Csv file must have the columns:
+#'                                      "table_name", "colum_name", "data_type", "is_required", "primary_key"
+#'                                      Note -
+#' @param sqlOutputPath                 File to write sql to.
+#' @param overwrite                     Boolean - overwrite existing file?
+#' @export
+#'
+#' @importFrom readr read_csv
+#' @return
+#'  string containing the sql for the table
+generateSqlSchema <- function(csvFilepath,
+                              sqlOutputPath = NULL,
+                              overwrite = FALSE) {
+  if (!is.null(sqlOutputPath) && (file.exists(sqlOutputPath) & !overwrite)) {
+    stop("Output file ", sqlOutputPath, "already exists. Set overwrite = TRUE to continue")
+  }
+
+  checkmate::assertFileExists(csvFilepath)
+  schemaDefinition <- readr::read_csv(csvFilepath, show_col_types = FALSE)
+  colnames(schemaDefinition) <- SqlRender::snakeCaseToCamelCase(colnames(schemaDefinition))
+  requiredFields <- c("tableName", "columnName", "dataType", "isRequired", "primaryKey")
+  checkmate::assertNames(colnames(schemaDefinition), must.include = requiredFields)
+
+  tableSqlStr <- "
+CREATE TABLE @database_schema.@table_prefix@table_name (
+  @table_fields
+);
+"
+  fullScript <- ""
+  defs <- "{DEFAULT @table_prefix = ''}\n"
+
+  for (table in unique(schemaDefinition$tableName)) {
+    tableFields <- schemaDefinition[schemaDefinition$tableName == table, ]
+    fieldDefinitions <- apply(tableFields, 1, .writeFieldDefinition)
+
+    primaryKeyFields <- tableFields[tableFields$primaryKey == "yes", ]
+    if (nrow(primaryKeyFields)) {
+      pkeyField <- paste0("\tPRIMARY KEY(", paste(primaryKeyFields$columnName, collapse = ","), ")")
+      fieldDefinitions <- c(fieldDefinitions, pkeyField)
+    }
+
+    fieldDefinitions <- paste(fieldDefinitions, collapse = ",\n")
+    tableString <- SqlRender::render(tableSqlStr,
+      table_name = paste0("@", table),
+      table_fields = fieldDefinitions
+    )
+
+    tableDefStr <- paste0("{DEFAULT @", table, " = ", table, "}\n")
+    defs <- paste0(defs, tableDefStr)
+
+    fullScript <- paste(fullScript, tableString)
+  }
+
+  # Get fields for each table
+  lines <- paste(defs, fullScript)
+  if (!is.null(sqlOutputPath)) {
+    writeLines(lines, sqlOutputPath)
+  }
+
+  lines
+}
