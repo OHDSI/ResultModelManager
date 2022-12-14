81782e5c77b5eb009efea7905728e52c8bb06220
Jamie Gilbert <JGilber2@its.jnj.com>
2022-12-13 16:17:28 -0800

    updated to use lower table names for checking table presence

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,6 +1,9 @@
 library(testthat)
 
 sqliteFile <- tempfile(fileext = "sqlite")
+unlink(sqliteFile)
+withr::defer(unlink(sqliteFile), testthat::teardown_env())
+
 connectionDetails <- DatabaseConnector::createConnectionDetails("sqlite", server = sqliteFile)
 connection <- DatabaseConnector::connect(connectionDetails)
 DatabaseConnector::disconnect(connection)
