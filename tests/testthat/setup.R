feda02b53144c942e46a0cac074bbe6bad6c0bbc
Jamie Gilbert <JGilber2@its.jnj.com>
2022-12-13 16:30:40 -0800

    keyring hack to get rid of warning in DatabaseConnector

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,10 +1,12 @@
 library(testthat)
 
 sqliteFile <- tempfile(fileext = "sqlite")
+keyring::key_set_with_value("rmm_test_db", password = sqliteFile)
+
 unlink(sqliteFile)
 withr::defer(unlink(sqliteFile), testthat::teardown_env())
 
-connectionDetails <- DatabaseConnector::createConnectionDetails("sqlite", server = sqliteFile)
+connectionDetails <- DatabaseConnector::createConnectionDetails("sqlite", server = keyring::key_get("rmm_test_db"))
 connection <- DatabaseConnector::connect(connectionDetails)
 DatabaseConnector::disconnect(connection)
 options(rstudio.connectionObserver.errorsSuppressed = TRUE)
