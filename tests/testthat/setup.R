4d646f8eb546ac6c3b5dd6fdfe4bec0ac4c7daef
Jamie Gilbert <JGilber2@its.jnj.com>
2022-12-15 11:53:51 -0800

    fixes for test on linux

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,12 +1,19 @@
 library(testthat)
 
+keyList <- keyring::keyring_list()
+
+if (!"hades-test-keyring" %in% keyList$keyring)
+  keyring::keyring_create("hades-test-keyring", password = "")
+
 sqliteFile <- tempfile(fileext = "sqlite")
-keyring::key_set_with_value("rmm_test_db", password = sqliteFile)
+keyring::key_set_with_value("rmm_test_db", password = sqliteFile, keyring = "hades-test-keyring")
 
 unlink(sqliteFile)
 withr::defer(unlink(sqliteFile), testthat::teardown_env())
 
-connectionDetails <- DatabaseConnector::createConnectionDetails("sqlite", server = keyring::key_get("rmm_test_db"))
+connectionDetails <- DatabaseConnector::createConnectionDetails("sqlite",
+                                                                server = keyring::key_get("rmm_test_db",
+                                                                                          keyring = "test-keyring"))
 connection <- DatabaseConnector::connect(connectionDetails)
 DatabaseConnector::disconnect(connection)
 options(rstudio.connectionObserver.errorsSuppressed = TRUE)
