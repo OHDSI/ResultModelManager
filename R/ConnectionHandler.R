e7a6a4f3f3a331368a88fbfd065a7953f2bad083
Jamie Gilbert <JGilber2@its.jnj.com>
2022-12-15 12:56:03 -0800

    Database connector v6.0.0 (#12)
    * support for database connector 6.0.0
    
    * support for dplyr tables loaded with dbplyr
    
    * simplified function definitions. Support for dplyr tables
    
    * Merged changes
    
    * missing import added
    
    * updated news and added develop version num
    
    * updated to use lower table names for checking table presence
    
    * keyring hack to get rid of warning in DatabaseConnector
    
    * Test coverage for use of tbl and dbms
    
    * Use of pool checkout when querying
    
    * fixes for test on linux

--- R/ConnectionHandler.R
+++ R/ConnectionHandler.R
@@ -50,6 +50,24 @@ ConnectionHandler <- R6::R6Class(
       }
     },
 
+    #' get dbms
+    #' @description Get the dbms type of the connection
+    dbms = function() {
+      DatabaseConnector::dbms(self$getConnection())
+    },
+
+    #' get table
+    #' @description get a dplyr table object (i.e. lazy loaded)
+    #' @param table                     table name
+    #' @param databaseSchema            databaseSchema to which table belongs
+    tbl = function(table, databaseSchema = NULL) {
+      checkmate::assertString(table)
+      checkmate::assertString(databaseSchema, null.ok = TRUE)
+      if (!is.null(databaseSchema))
+        table <- paste0(databaseSchema, ".", table)
+
+      dplyr::tbl(self$getConnection(), table)
+    },
     #' Render Translate Sql.
     #' @description
     #' Masked call to SqlRender
--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -1,7 +1,16 @@
 library(testthat)
 
 sqliteFile <- tempfile(fileext = "sqlite")
-connectionDetails <- DatabaseConnector::createConnectionDetails("sqlite", server = sqliteFile)
+.varFile <- ".SQLITE_PATH"
+unlink(.varFile)
+
+writeLines(sqliteFile, .varFile)
+
+unlink(sqliteFile)
+withr::defer(unlink(sqliteFile), testthat::teardown_env())
+
+connectionDetails <- DatabaseConnector::createConnectionDetails("sqlite",
+                                                                server = readLines(".SQLITE_PATH"))
 connection <- DatabaseConnector::connect(connectionDetails)
 DatabaseConnector::disconnect(connection)
 options(rstudio.connectionObserver.errorsSuppressed = TRUE)
