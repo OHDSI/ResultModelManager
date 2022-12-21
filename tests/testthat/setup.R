a73013cd6cd5ace067d1997d62a0ded84f2fefde
Jamie Gilbert <JGilber2@its.jnj.com>
2022-12-21 13:59:44 -0800

    upload added - including working unit tests

--- tests/testthat/setup.R
+++ tests/testthat/setup.R
@@ -5,12 +5,31 @@ sqliteFile <- tempfile(fileext = "sqlite")
 unlink(.varFile)
 
 writeLines(sqliteFile, .varFile)
-
 unlink(sqliteFile)
-withr::defer(unlink(sqliteFile), testthat::teardown_env())
+withr::defer({
+  unlink(sqliteFile)
+  unlink(.varFile)
+}, testthat::teardown_env())
 
 connectionDetails <- DatabaseConnector::createConnectionDetails("sqlite",
                                                                 server = readLines(".SQLITE_PATH"))
 connection <- DatabaseConnector::connect(connectionDetails)
 DatabaseConnector::disconnect(connection)
 options(rstudio.connectionObserver.errorsSuppressed = TRUE)
+
+testSchema <- Sys.getenv("CDM5_POSTGRESQL_OHDSI_SCHEMA")
+
+if (dir.exists(Sys.getenv("DATABASECONNECTOR_JAR_FOLDER"))) {
+  jdbcDriverFolder <- Sys.getenv("DATABASECONNECTOR_JAR_FOLDER")
+} else {
+  jdbcDriverFolder <- "~/.jdbcDrivers"
+  Sys.setenv(DATABASECONNECTOR_JAR_FOLDER = jdbcDriverFolder)
+  dir.create(jdbcDriverFolder, showWarnings = FALSE)
+  DatabaseConnector::downloadJdbcDrivers("postgresql", pathToDriver = jdbcDriverFolder)
+  withr::defer(
+  {
+    unlink(jdbcDriverFolder, recursive = TRUE, force = TRUE)
+  },
+    testthat::teardown_env()
+  )
+}
