b8f4e13f6cb0f31a945b94098ef434f55a811e6b
jgilber2 <jgilber2@its.jnj.com>
2025-09-19 14:29:04 -0700

    Add type checks in results files

--- tests/testthat/sql/getResults.sql
+++ tests/testthat/sql/getResults.sql
@@ -1,1 +1,4 @@
-SELECT * FROM @result_schema.@cohort where cohort_id = @cohort_id
+{TYPEC INT[] @cohort_id}
+{TYPEC TEXT @cohort}
+
+SELECT * FROM @result_schema.@cohort where cohort_id IN (@cohort_id)
